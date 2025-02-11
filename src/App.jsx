bce7293ad2a06b0ecc5c5b4bfa9c1c87420d7e50
Alexander Bouthillet <alex.bouthillet@gmail.com>
2025-02-11 12:00:56 -0500

    add homepage and fetch api to render all puppies and their names.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
+import { Routes, Route } from 'react-router-dom';
+
+import Homepage from './HomePage.jsx';
 
 const App = () => {
 
   return (
     <>
-    <h1>Puppy Bowl</h1>
+      <h1>Puppy Bowl</h1>
+
+      <Routes>
+        <Route path='/' element={<Homepage />}/>
+        <Route path='/puppy-details/:id' element={<h2>Puppy Details</h2>}/>
+      </Routes>
     </>
   )
 }
---
+++ src/HomePage.jsx
@@ -0,0 +1,42 @@
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+
+const Homepage = () => {
+  const [ puppies, setPuppies ] = useState([]);
+
+  useEffect(() => {
+    
+    const getAllPuppies = async() => {
+      const response = await fetch('https://fsa-puppy-bowl.herokuapp.com/api/2501-ftb-et-web-ft/players');
+      // console.log(response)
+      const jsonObj = await response.json();
+      // console.log(jsonObj)
+      const allPuppies = jsonObj.data.players
+      console.log(allPuppies)
+
+      setPuppies(allPuppies);
+
+    }
+    getAllPuppies();
+  }, []);
+
+  return(
+    <>
+      <h2>Homepage</h2>
+      {
+        puppies.map((singlePuppy) => {
+          
+          return (
+            <Link key={singlePuppy.id} to={`/puppy-details/${singlePuppy.id}`}>
+              <h2>{singlePuppy.name}</h2>
+
+              <img src={singlePuppy.imageUrl} height={200} width={200}/>
+            </Link>
+          )
+        })
+      }
+    </>
+  )
+}
+
+export default Homepage;
